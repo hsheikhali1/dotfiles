1ade6c12e7f9718b29b9aa442902f56afd218c0b
Harun Sheikhali <sufisheikhali@gmail.com>
2024-06-11 22:48:06 -0400

    move flake.nix and flake.lock to root of dotfiles

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./hosts/default/configuration.nix
+        ./nixos/hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
     };
